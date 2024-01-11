2ccf14a4e1e5adb1397e56e05bca755408d7f2e8
Elliot <elliot.sequeira@gmail.com>
2024-01-11 05:40:47 +0000

    Added new tests

--- src/test_cardGameClient.py
+++ src/test_cardGameClient.py
@@ -15,6 +15,18 @@ def test_join_game():
     assert response.status_code == 200
     assert response.json() == {"message": "Player2 has joined the game."}
 
+
+def test_join_game_twice():
+    client.post("/api/games/", json={"name": "Player2"})
+    response = client.post("/api/games/1/join/", json={"name": "Player2"})
+    assert response.status_code == 200
+    assert response.json() == {"message": "Player2 has joined the game."}
+    client.post("/api/games/", json={"name": "Player2"})
+    response = client.post("/api/games/1/join/", json={"name": "Player2"})
+    assert response.status_code == 200
+    assert response.json() == {"message": "Player2 has joined the game."}
+
+
 def test_start_game():
     client.post("/api/games/", json={"name": "Player1"})
     client.post("/api/games/1/join/", json={"name": "Player2"})
